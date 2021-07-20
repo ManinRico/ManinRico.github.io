333714bb1ddacfe0a29a0a92aabcdab5bfe9d07a
construccionesmanin <86809323+construccionesmanin@users.noreply.github.com>
2021-07-20 12:17:39 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+/* Project: Web desing for Construcciones Manin, S.L - Author: Jose Maria Rico Leal - Javascript para el contact-form */
+var form = document.getElementById("contact-form");
+
+
+$(document).ready(function() {
+	$("#contact-form").submit(function(event) {
+
+		var is_error = false;
+		var error_count = 0;
+	
+
+		var first_name = $("#first_name").val();
+		if (first_name == "") {
+			$("input#first_name").css("border-color", "#ff3333");
+			is_error = true;
+			error_count += 1;
+			
+			
+		}
+
+		
+		var email = $("#email").val();
+		if (email == "") {
+			$("input#email").css("border-color", "#ff3333");
+			is_error = true;
+			error_count += 1;
+		
+		}
+		
+
+		if (is_error == true) {
+			$("span#error-count").text(error_count);
+			$("p#error-list").css("display", "block");
+			event.preventDefault();			
+		}
+
+		
+		$("#first_name").focusout(function(){
+			$("input#first_name").css("border-color", "#dddddd");
+		});
+		
+		$("#email").focusout(function(){
+			$("input#email").css("border-color", "#dddddd");
+		});
+		
+		
+	});
+});
+
+ function print_out_error() {
+            let inputFields = document.getElementsByClassName('form_input');
+            let message =''; 
+            for (var i = 0; i < inputFields.length; i++) {
+                console.log(inputFields[i].value);
+                if (inputFields[i].value === '') {
+                        message = message + inputFields[i].getAttribute('name') + ' y ';
+                }
+            }
+
+            let n = message.lastIndexOf("y");
+            message = message.substring(0,n); 
+
+            document.querySelector('#fail-message').innerHTML =
+                'Por favor rellena tu ' +
+                message +
+                ' lo necesitamos para contactarte ;)!!!';
+        }
+
+
+
+
